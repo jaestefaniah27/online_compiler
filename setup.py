4807d72c4661d1ffc8901705c7ed1e8572be8d46
Jorgerente <jaestefaniah27@gmail.com>
2025-05-25 20:01:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='arcompile',
     version='0.1.0',
-    packages=find_packages(),
-    py_modules=['arcompile'],
+    py_modules=['arcompile'],  # 👈 NO 'src.arcompile' ni nada raro
     install_requires=[],
     entry_points={
         'console_scripts': [
-            'arcompile = arcompile:main'
+            'arcompile = arcompile:main'  # 👈 main() debe existir en arcompile.py
         ],
     },
-    author='jaestefaniah27',
-    description='Compilador remoto de sketches Arduino con ESP32 y flasheo local',
-    url='https://github.com/jaestefaniah27/online_compiler',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-    ],
-    python_requires='>=3.6',
 )
