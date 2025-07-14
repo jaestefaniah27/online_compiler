96db4c9742ac21d8b94175a88dd019cf65fcc0ce
Jorgerente <jaestefaniah27@gmail.com>
2025-07-14 11:22:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
-
+from arcompile_version import __version__ as VERSION
 def get_version():
     with open("version.txt", "r", encoding="utf-8") as f:
         return f.read().strip()
         
 setup(
     name='arcompile',
-    version=get_version(),
+    version=VERSION,
     py_modules=['arcompile'],  # 👈 NO 'src.arcompile' ni nada raro
     install_requires=[],
     entry_points={
